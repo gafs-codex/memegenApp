d930170fbc89c30a15267dd3d5bb4fad340602b3
gafs-codex <abdulmuizgafarcodes@gmail.com>
2026-04-11 17:16:03 +0100

    meme generator mini project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import Header from './components/Header'
+import Main from './components/Main'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
+export default App;
+
---
+++ src/components/Main.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+function Main() {
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    function handleChange(event) {
+        const { value, name } = event.currentTarget
+        // console.log(value);
+        setMeme((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+    return (
+        <main>
+            <div className="form">
+
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
+export default Main;
